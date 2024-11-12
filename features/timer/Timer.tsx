ab5452de4bb3f1800d8707f09276f8a08db2dd4a
kaitokosuge <kaitokosuge.mi@gmail.com>
2024-11-12 15:26:55 +0900

    add : ci format

--- features/timer/Timer.tsx
+++ features/timer/Timer.tsx
@@ -14,6 +14,7 @@ export default function Timer() {
         isActive,
         isPaused,
     } = useTimer();
+    console.log('hello');
     return (
         <div className="text-center">
             <TimeView seconds={seconds} />
